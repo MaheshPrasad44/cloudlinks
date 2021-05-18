ca11268e67f841534a99404f72df1f7a8e039820
maheshprasad4498 <maheshprasad4498@gmail.com>
2021-05-18 00:24:54 -0400

    adding files

---
+++ cloudlinks/cloudlinks/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div className="App">
+     <h1>Infrastructure and CI/CD - Cloud Project Repository Links</h1>
+    </div>
+    <table>
+  <tr>
+    <th>Name</th>
+    <th>Link</th>
+  
+  </tr>
+  <tr>
+    <td>IaC_Infrastructure-as-Code</td>
+    <td><a href="https://github.com/MaheshPrasad44/IaC_Infrastructure-as-Code">IaC Link</a></td>
+ 
+  </tr>
+  <tr>
+  <td>FaaS_Function-as-a-Service</td>
+    <td><a href="https://github.com/MaheshPrasad44/FaaS_Function-as-a-Service">FaaS Link</a></td>
+ 
+   
+  </tr>
+  <tr>
+  <td>AMI-AWS</td>
+    <td><a href="https://github.com/MaheshPrasad44/AMI-AWS">AMI-AWS Link</a></td>
+ 
+   
+  </tr>
+  <tr>
+  <td>E-Commerce_Book_WebApp</td>
+    <td><a href="https://github.com/MaheshPrasad44/E-Commerce_Book_WebApp">App Link</a></td>
+ 
+   
+  </tr>
+</table>
+    </div>
+  );
+}
+
+export default App;
